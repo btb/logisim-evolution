922d947eef6fa3cdb9d4170890d6bad0c5f767e9
Bradley Bell <bradley.bell@gmail.com>
2023-10-10 18:30:17 -0700

    Use new paintFlipFlop

--- src/main/java/com/cburch/logisim/std/ttl/Ttl74175.java
+++ src/main/java/com/cburch/logisim/std/ttl/Ttl74175.java
@@ -16,6 +16,10 @@ import com.cburch.logisim.instance.InstancePainter;
 import com.cburch.logisim.instance.InstanceState;
 import java.awt.Graphics;
 
+/**
+ * TTL 74x175 quadruple D-type flip-flops with asynchronous reset
+ * Model based on < href="https://www.ti.com/lit/ds/symlink/sn74ls175.pdf">74LS175 datasheet</a>.
+ */
 public class Ttl74175 extends AbstractTtlGate {
   /**
    * Unique identifier of the tool, used as reference in project files. Do NOT change as it will
@@ -84,96 +88,50 @@ public class Ttl74175 extends AbstractTtlGate {
     g.drawLine(x + 80, y + 30, x + 150, y + 30);
 
     // dff1
-    g.drawRect(x + 57, y + 33, 6, 12);
-
-    g.drawOval(x + 59, y + 45, 2, 2);
+    Drawgates.paintFlipFlop(g, x + 60, y, true, false, true);
+    Drawgates.paintOutputgate(g, x + 30, y, x + 57, y + 37, false, height);
+    Drawgates.paintOutputgate(g, x + 50, y, x + 55, y + 41, false, height);
+    Drawgates.paintSingleInputgate(g, x + 70, y, x + 63, y + 37, false, height);
     g.fillOval(x + 59, y + 49, 2, 2);
     g.drawLine(x + 60, y + 47, x + 60, y + 50);
-
-    g.drawOval(x + 55, y + 40, 2, 2);
-    g.drawLine(x + 50, y + height - AbstractTtlGate.PIN_HEIGHT, x + 50, y + 41);
-    g.drawLine(x + 50, y + 41, x + 55, y + 41);
-
-    g.drawLine(x + 30, y + height - AbstractTtlGate.PIN_HEIGHT, x + 30, y + 37);
-    g.drawLine(x + 30, y + 37, x + 57, y + 37);
-
-    g.drawLine(x + 70, y + height - AbstractTtlGate.PIN_HEIGHT, x + 70, y + 37);
-    g.drawLine(x + 63, y + 37, x + 70, y + 37);
-
     g.drawLine(x + 61, y + 41, x + 63, y + 42);
     g.drawLine(x + 61, y + 41, x + 63, y + 40);
-    g.drawString("D", x + 64, y + 36);
-    g.drawString("Q", x + 52, y + 36);
     g.drawLine(x + 63, y + 41, x + 97, y + 41);
     g.drawLine(x + 80, y + 30, x + 80, y + 41);
     g.fillOval(x + 79, y + 29, 2, 2);
     g.fillOval(x + 79, y + 40, 2, 2);
-    // dff2
-    g.drawRect(x + 97, y + 33, 6, 12);
 
-    g.drawOval(x + 99, y + 45, 2, 2);
+    // dff2
+    Drawgates.paintFlipFlop(g, x + 100, y, true, false, false);
+    Drawgates.paintOutputgate(g, x + 130, y, x + 103, y + 37, false, height);
+    Drawgates.paintOutputgate(g, x + 110, y, x + 105, y + 41, false, height);
+    Drawgates.paintSingleInputgate(g, x + 90, y, x + 97, y + 37, false, height);
     g.fillOval(x + 99, y + 49, 2, 2);
     g.drawLine(x + 100, y + 47, x + 100, y + 50);
-
     g.drawOval(x + 103, y + 40, 2, 2);
-    g.drawLine(x + 110, y + height - AbstractTtlGate.PIN_HEIGHT, x + 110, y + 41);
-    g.drawLine(x + 105, y + 41, x + 110, y + 41);
-
-    g.drawLine(x + 130, y + height - AbstractTtlGate.PIN_HEIGHT, x + 130, y + 37);
-    g.drawLine(x + 130, y + 37, x + 103, y + 37);
-
-    g.drawLine(x + 90, y + height - AbstractTtlGate.PIN_HEIGHT, x + 90, y + 37);
-    g.drawLine(x + 90, y + 37, x + 97, y + 37);
-
     g.drawLine(x + 97, y + 42, x + 99, y + 41);
     g.drawLine(x + 97, y + 40, x + 99, y + 41);
 
-    g.drawString("D", x + 92, y + 36);
-    g.drawString("Q", x + 104, y + 36);
-
     // dff3
-    g.drawRect(x + 97, y + 15, 6, 12);
-
-    g.drawOval(x + 99, y + 13, 2, 2);
+    Drawgates.paintFlipFlop(g, x + 100, y, true, true, false);
+    Drawgates.paintOutputgate(g, x + 130, y, x + 103, y + 23, true, height);
+    Drawgates.paintOutputgate(g, x + 110, y, x + 105, y + 19, true, height);
+    Drawgates.paintSingleInputgate(g, x + 90, y, x + 97, y + 23, true, height);
     g.fillOval(x + 99, y + 9, 2, 2);
     g.drawLine(x + 100, y + 13, x + 100, y + 10);
-
     g.drawOval(x + 103, y + 18, 2, 2);
-    g.drawLine(x + 110, y + AbstractTtlGate.PIN_HEIGHT, x + 110, y + 19);
-    g.drawLine(x + 105, y + 19, x + 110, y + 19);
-
-    g.drawLine(x + 130, y + AbstractTtlGate.PIN_HEIGHT, x + 130, y + 23);
-    g.drawLine(x + 130, y + 23, x + 103, y + 23);
-
-    g.drawLine(x + 90, y + AbstractTtlGate.PIN_HEIGHT, x + 90, y + 23);
-    g.drawLine(x + 90, y + 23, x + 97, y + 23);
-
     g.drawLine(x + 97, y + 20, x + 99, y + 19);
     g.drawLine(x + 97, y + 18, x + 99, y + 19);
 
-    g.drawString("D", x + 92, y + 29);
-    g.drawString("Q", x + 104, y + 29);
-
     // dff4
-    g.drawRect(x + 57, y + 15, 6, 12);
-
-    g.drawOval(x + 59, y + 13, 2, 2);
+    Drawgates.paintFlipFlop(g, x + 60, y, true, true, true);
+    Drawgates.paintOutputgate(g, x + 30, y, x + 57, y + 23, true, height);
+    Drawgates.paintOutputgate(g, x + 50, y, x + 55, y + 19, true, height);
+    Drawgates.paintSingleInputgate(g, x + 70, y, x + 63, y + 23, true, height);
     g.drawLine(x + 60, y + 13, x + 60, y + 10);
-
     g.drawOval(x + 55, y + 18, 2, 2);
-    g.drawLine(x + 50, y + AbstractTtlGate.PIN_HEIGHT, x + 50, y + 19);
-    g.drawLine(x + 50, y + 19, x + 55, y + 19);
-
-    g.drawLine(x + 30, y + AbstractTtlGate.PIN_HEIGHT, x + 30, y + 23);
-    g.drawLine(x + 30, y + 23, x + 57, y + 23);
-
-    g.drawLine(x + 70, y + AbstractTtlGate.PIN_HEIGHT, x + 70, y + 23);
-    g.drawLine(x + 63, y + 23, x + 70, y + 23);
-
     g.drawLine(x + 61, y + 19, x + 63, y + 20);
     g.drawLine(x + 61, y + 19, x + 63, y + 18);
-    g.drawString("D", x + 64, y + 29);
-    g.drawString("Q", x + 52, y + 29);
     g.drawLine(x + 63, y + 19, x + 97, y + 19);
     g.drawLine(x + 80, y + 19, x + 80, y + 40);
     g.fillOval(x + 79, y + 18, 2, 2);
